34063bb04243dfc969599914e864dd34b0893255
Виталий <klvaek0@gmail.com>
2025-12-28 15:56:52 +0700

    deleted incorrected files in bot - it was be most cause of errors

--- bot/bot/src/commands/exports.ts
+++
@@ -1,7 +0,0 @@
-export * from '../commands/vpnCommands.js';
-export * from './subscrise.js';
-export * from '../index.js';
-export * from '../commands/profile.js';
-export * from '../services/help.js';
-export * from './productsCommand.js';
-export * from './payments.js';
--- bot/bot/src/services/help.ts
+++
@@ -1,9 +0,0 @@
-import { MyContext } from "../types.js";
-import { InlineKeyboard, CallbackQueryContext, Bot } from "grammy";
-
-export const help = async (ctx: CallbackQueryContext<MyContext>) => {
-  await ctx.callbackQuery.message?.editText('Напишите ваше обращение, вам ответит первый освободившийся специалист!', {
-    reply_markup: new InlineKeyboard().text('< На главную', 'back'),
-  });
-  await ctx.answerCallbackQuery();
-}
